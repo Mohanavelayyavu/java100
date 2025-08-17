b9bdd8deb84bf803eb97b6717bd5b4376ba9c92b
Mohanavelayyavu <168104003+Mohanavelayyavu@users.noreply.github.com>
2025-08-17 11:55:41 +0530

    Create 217. Contains Duplicate.java

---
+++ 217. Contains Duplicate.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) 
+    {
+        Set<Integer> n = new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            n.add(nums[i]);
+        }
+        if(n.size()==nums.length)
+        {
+            return false;
+        }
+         return true;
+    }
+}
